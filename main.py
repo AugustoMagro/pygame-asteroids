7512f60032dcb287ea897899ee5c5697b0295e95
AugustoMagro <augustovmagro@gmail.com>
2025-05-27 23:07:02 -0300

    Playre
     done

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import pygame
 from constants import *
+from player import Player
 
 class Game:
     def __init__(self):
@@ -7,6 +8,12 @@ class Game:
         print("Starting Asteroids!")
         print(f"Screen width: {SCREEN_WIDTH}")
         print(f"Screen height: {SCREEN_HEIGHT}")
+        
+        self.clock = pygame.time.Clock()
+        self.dt = 0
+        x = SCREEN_WIDTH/2
+        y = SCREEN_HEIGHT/2
+        self.player = Player(x,y)
         self.main()
 
     def main(self):
@@ -15,7 +22,10 @@ class Game:
                 if event.type == pygame.QUIT:
                     return
             self.screen.fill((0, 0, 0, 0.5), rect=None, special_flags=0)
+            self.player.draw(self.screen)
+            self.player.update(self.dt)
             pygame.display.flip()
+            self.dt = self.clock.tick(60)/1000
 
 if __name__ == "__main__":
     Game()
